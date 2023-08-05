08d35c3b098ec1743207f888e3a3ac570019f6ab
André Domingues <dominguesstroppa@gmail.com>
2023-08-05 17:44:04 -0300

    Add Projects section

--- src/components/projects/Projects.vue
+++ src/components/projects/Projects.vue
@@ -4,10 +4,97 @@
 <template>
   <div>
     <NotepadBody title="Projects" target="projects">
-        <h1>Project</h1> 
+      <h1 class="font-bold text-gray-800 text-lg mt-4">Meus Projetos!</h1>
+      <p class="mt-3 text-gray-600">Na minha jornada como desenvolvedor web, tive a oportunidade de criar e participar de diversos projetos. Alguns deles foram realizados de forma independente, enquanto outros surgiram durante cursos. Abaixo, destaco alguns desses projetos. Se desejar explorar mais detalhes sobre os projetos, confira o meu <a href="https://github.com/andrefelipe18" target="_blank" class="link">GitHub aqui</a>.</p>
+
+      <hr class="mt-2">
+
+      <h1 class="font-bold text-gray-600 text-lg mt-2">VFull & VFullAuth</h1>
+      <p class="mt-3 text-gray-600">Criei os starter kits de VueJS, VFull e VFullAuth, inicialmente para uso pessoal. Depois, disponibilizei-os. VFull oferece uma variedade de ferramentas para o desenvolvimento em VueJS, enquanto o VFullAuth, além dessas características, já vem com autenticação do Laravel Breeze pronta para uso.</p>
+      <p class="text-gray-600">
+        Link para o repostório: 
+        <a class="link" href="https://github.com/andrefelipe18/VFull" target="_blank" rel="noopener noreferrer">VFull</a> e 
+        <a class="link" href="https://github.com/andrefelipe18/VFullAuth" target="_blank" rel="noopener noreferrer">VFullAuth</a>
+      </p>
+      <div class="flex justify-center">        
+        <img src="/vfull.png" class="h-14" alt="Vfull Icon">        
+      </div>
+
+      <hr class="mt-2">
+
+      <h1 class="font-bold text-gray-600 text-lg mt-2">CashTrack</h1>
+      <p class="mt-3 text-gray-600">CashTrack é um aplicativo web para controle de finanças pessoais. O projeto foi desenvolvido para uso pessoal, mas disponibilizei o código fonte no GitHub. O projeto foi desenvolvido com Laravel e VueJS.</p>
+      <p class="text-gray-600">
+        Link para o repostório: 
+        <a class="link" href="https://github.com/andrefelipe18/CashTrack" target="_blank" rel="noopener noreferrer">CashTrack</a>
+      </p>
+      <div class="flex justify-center">        
+        <img src="/cashtrack.svg" class="mt-2 h-8" alt="CashTrack Icon">
+      </div>
+
+      <hr class="mt-2">
+
+      <h1 class="font-bold text-gray-600 text-lg mt-2">Aprenda-Mysqli-PDO</h1>
+      <p class="mt-3 text-gray-600">Aprenda-Mysqli-PDO é um repostório que contem conteúdo para ajudar novos desenvolvedores a aprender a trabalhar com bancos de dados usando MySQLi e PDO.</p>
+      <p class="text-gray-600">
+        Link para o repostório: 
+        <a class="link" href="https://github.com/andrefelipe18/Aprenda-Mysqli-PDO" target="_blank" rel="noopener noreferrer">Aprenda-Mysqli-PDO</a>
+      </p>
+      <div class="flex justify-center">        
+        <span class="text-xl">📚</span>
+      </div>
+
+      <hr class="mt-2">
+
+      <h1 class="font-bold text-gray-600 text-lg mt-2">adonis-api</h1>
+      <p class="mt-3 text-gray-600">adonis-api é um projeto desenvolvido durante o curso de AdonisJS da Cataline. O projeto consiste em uma API RESTful para gerenciamento de usuários e autenticação.</p>
+      <p class="text-gray-600">
+        Link para o repostório: 
+        <a class="link" href="https://github.com/andrefelipe18/adonis-api" target="_blank" rel="noopener noreferrer">adonis-api</a>
+      </p>
+      <div class="flex justify-center">        
+        <img src="https://avatars.githubusercontent.com/u/13810373?s=280&v=4" class="mt-2 h-8" alt="Adonis Icon">
+      </div>
+
+      <hr class="mt-2">
+
+      <h1 class="font-bold text-gray-600 text-lg mt-2">locadora_de_Carros</h1>
+      <p class="mt-3 text-gray-600">locadora_de_Carros é um projeto desenvolvido durante um curso de Laravel. O projeto consiste em um sistema de locação de carros com uma API Rest em Laravel e o Front end em VueJS</p>
+      <p class="text-gray-600">
+        Link para o repostório: 
+        <a class="link" href="https://github.com/andrefelipe18/locadora_de_Carros" target="_blank" rel="noopener noreferrer">locadora_de_Carros</a>
+      </p>
+      <div class="flex justify-center">        
+        <img class="mt-2 h-8" src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/vuejs/vuejs-original.svg" alt="VueJS Icon" />
+        <img class="mt-2 h-8" src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/laravel/laravel-plain.svg" alt="Laravel Icon" />        
+      </div>
+
+      <hr class="mt-2">
+
+      <h1 class="font-bold text-gray-600 text-lg mt-2">Super-Gestao</h1>
+      <p class="mt-3 text-gray-600">Projeto realizado durante o curso de Laravel foi tratado os conceitos de Rotas, Controllers, Views, Models, Migrations, Seeders, Factories, MySQL, Tinker, Eloquent ORM, Middlewares e Autenticação Web do Framework Laravel</p>
+      <p class="text-gray-600">
+        Link para o repostório: 
+        <a class="link" href="https://github.com/andrefelipe18/Super-Gestao" target="_blank" rel="noopener noreferrer">Super-Gestao</a>
+      </p>
+      <div class="flex justify-center">        
+        <img class="mt-2 h-8" src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/laravel/laravel-plain.svg" alt="Laravel Icon" />
+      </div>
+
+      <hr class="mt-2">
+
+      <p class="mt-2 text-gray-600">
+        Esses são alguns dos projetos que desenvolvi. Se desejar explorar mais detalhes sobre os projetos, confira o meu <a href="https://github.com/andrefelipe18" target="_blank" class="link">GitHub aqui</a>.
+      </p>
+      <div class="flex justify-center">        
+        <img class="mt-2 h-8" src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/github/github-original.svg" alt="Github icon" />
+      </div>
     </NotepadBody>    
   </div>
 </template>
 <style scoped>
-
+.link{
+  color: #3b82f6;
+  text-decoration: underline;
+}
 </style>
