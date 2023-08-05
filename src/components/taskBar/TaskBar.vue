e8188daa6dbc93b23d9bf05beed02c02f9236a23
André Domingues <dominguesstroppa@gmail.com>
2023-08-05 18:02:00 -0300

    Add Icons TaskBar

--- src/components/taskBar/TaskBar.vue
+++ src/components/taskBar/TaskBar.vue
@@ -60,27 +60,53 @@ watch(notepadOpen, (value) => {
 </script>
 <template>
   <section>
-    <div class="container-logo-windows">
-        <svg class="logo-windows" xmlns="http://www.w3.org/2000/svg" width="16" height="16" viewBox="0 0 16 16"><path fill="currentColor" d="M6.555 1.375L0 2.237v5.45h6.555V1.375zM0 13.795l6.555.933V8.313H0v5.482zm7.278-5.4l.026 6.378L16 16V8.395H7.278zM16 0L7.33 1.244v6.414H16V0z"/></svg>
+    <div class="left-icons">
+      <div class="container-logo-windows">
+          <svg class="logo-windows" xmlns="http://www.w3.org/2000/svg" width="16" height="16" viewBox="0 0 16 16"><path fill="currentColor" d="M6.555 1.375L0 2.237v5.45h6.555V1.375zM0 13.795l6.555.933V8.313H0v5.482zm7.278-5.4l.026 6.378L16 16V8.395H7.278zM16 0L7.33 1.244v6.414H16V0z"/></svg>
+      </div>
+      <div class="apps">
+          <ul class="apps-list">
+              <li v-for="app in minimizeApps" :key="app" class="app" >
+                  <a v-if="app === 'explorer'" @click="openExplorer" id="explorerMinimize" @touchstart.passive="openExplorer">
+                    <img  src="/Windows_Explorer.svg" class="icon">
+                  </a>
+                  <router-link v-else :to="`/${app}`" @click="openNotepad" id="notepadMinimize" @touchstart.passive="openNotepad">
+                    <img src="/notepad-icon.webp" class="icon">
+                  </router-link>
+              </li>
+          </ul>
+      </div>
     </div>
-    <div class="apps">
-        <ul class="apps-list">
-            <li v-for="app in minimizeApps" :key="app" class="app" >
-                <a v-if="app === 'explorer'" @click="openExplorer" id="explorerMinimize" @touchstart.passive="openExplorer">
-                  <img  src="/Windows_Explorer.svg" class="icon">
-                </a>
-                <router-link v-else :to="`/${app}`" @click="openNotepad" id="notepadMinimize" @touchstart.passive="openNotepad">
-                  <img src="/notepad-icon.webp" class="icon">
-                </router-link>
-            </li>
-        </ul>
+    <div class="right-icons">
+      <ul class="flex items-center">
+        <li>
+          <svg xmlns="http://www.w3.org/2000/svg" class="icon h-3" viewBox="0 0 512 512"><path fill="white" d="M256 63.6L0 319.6l69.8 69.8L256 203.2l186.2 186.2l69.8-69.8z"/></svg>
+        </li>
+        <li class="ml-2">
+          <img src="/volume.png" class="icon">
+        </li>
+        <li class="ml-1">
+          <img src="/wifi.png" class="icon">
+        </li>
+        <li class="ml-1">
+          {{ new Date().toLocaleTimeString() }}
+        </li>
+        <li class="ml-2">
+          <svg xmlns="http://www.w3.org/2000/svg" class="icon h-3" viewBox="0 0 15 15"><path fill="none" stroke="white" stroke-linecap="square" stroke-linejoin="round" d="m5.5 11.493l2 2.998l2-2.998h4a1 1 0 0 0 1-1V1.5a.999.999 0 0 0-1-.999h-12a1 1 0 0 0-1 1v8.993a1 1 0 0 0 1 1h4Z" clip-rule="evenodd"/></svg>
+        </li>
+      </ul>
     </div>
   </section>
 </template>
 <style scoped>
+/* section {
+  @apply fixed bottom-0 min-h-[50px] bg-[#0C0C0C] w-full
+  z-50 flex items-center
+} */
+
 section {
   @apply fixed bottom-0 min-h-[50px] bg-[#0C0C0C] w-full
-  z-50 flex items-center  
+  z-50 grid items-center grid-cols-8
 }
 
 section{
@@ -91,8 +117,9 @@ section{
     -ms-user-select: none;
     user-select: none;
 }
+
 .container-logo-windows {
-  @apply m-4 
+  @apply m-4
 }
 .logo-windows {
   @apply max-w-[20px] hover:cursor-pointer transition-all duration-300
@@ -107,7 +134,7 @@ section{
 }
 
 .apps {
-  @apply flex items-center justify-center ml-2
+  @apply flex items-center justify-start ml-2 
 }
 
 .apps-list {
@@ -123,12 +150,11 @@ section{
   hover:opacity-80 hover:scale-110 
 }
 
+.left-icons {
+  @apply flex items-center justify-start ml-2 col-span-7
+}
+
+.right-icons {
+  @apply flex items-center justify-center ml-2
+}
 </style>
-<!--  console.log('ABERTO');
-    const notepadLink = document.querySelector('#notepadMinimize') as HTMLElement    
-    if(notepadLink) {
-        console.log('ABERTO');
-        const notepad = notepadLink.parentElement as HTMLElement
-        notepad.style.backgroundColor = '#474747'
-        notepad.style.borderBottom = '1px solid #83c0ef'
-    } -->
